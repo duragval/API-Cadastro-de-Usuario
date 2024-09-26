f821fe921ec87a20cf2d2c31d199106865742c32
duragval <varleioli@gmail.com>
2024-09-26 20:37:24 -0300

    Alteração Docker Correto

--- CadastroUsuarioWebApi/Context/AppDbContext.cs
+++ CadastroUsuarioWebApi/Context/AppDbContext.cs
@@ -7,6 +7,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+        this.Database.EnsureCreated();
     }
 
     public DbSet<Cadastro>? Cadastros { get; set; }
